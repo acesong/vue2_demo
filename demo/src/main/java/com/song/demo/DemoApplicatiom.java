5585d248136c4693b3e47c77e701753db803feda
song <799098819@qq.com>
2020-09-28 22:50:03 +0800

    the first update

---
+++ demo/src/main/java/com/song/demo/DemoApplicatiom.java
@@ -0,0 +1,12 @@
+package com.song.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplicatiom {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplicatiom.class);
+
+    }
+}
